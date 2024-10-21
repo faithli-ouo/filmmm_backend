dabaefc0afe54cc11880a85e810daaec0b53ba8c
root <128820090+faithli0929@users.noreply.github.com>
2024-10-21 22:52:50 +0800

    Added Movies Other Service

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { QueryResponse } from 'src/types/global/global.type';
 import { MovieResultObject } from 'src/types/movies/movies.types';
+import { MovieDetailsObject } from 'src/types/movies/movies_details_type';
+import { MoviesController } from './movies.controller';
 
 export type MovieList = 'now_playing' | 'popular' | 'top_rated' | 'upcoming';
 
@@ -30,7 +32,7 @@ export class MoviesService {
       });
 
     return res;
-  }
+  };
 
   //Get Movie Details by defined id
   async getMoviesDetailsByID(id: number): Promise<MovieDetailsObject> {
@@ -52,12 +54,13 @@ export class MoviesService {
       });
 
     return res;
-  }
+  };
 
   //Get Movie Alternative Titles by deifned id
   async getMoviesAlternativeTitlesByID(
     id: number,
-  ): Promise<MovieDetailsObject> {
+  ): Promise<MoviesController> {
+
     const url = `${originUrl}${id}/alternative_titles?language=${language}&region=${region}`;
     const options = {
       method: 'GET',
@@ -76,5 +79,355 @@ export class MoviesService {
       });
 
     return res;
-  }
+  };
+
+  //Get Movie Changes by deifned id
+  async getMoviesChangesByID(
+      id: number,
+    ): Promise<MoviesController> {
+      
+      const url = `${originUrl}${id}/changes?language=${language}&region=${region}`;
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: process.env.TMDB_Token,
+        },
+      };
+  
+      const res = await fetch(url, options)
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+  
+      return res;
+    };
+
+  //Get Movie Credits by deifned id
+  async getMoviesCreditsByID(
+    id: number,
+  ): Promise<MoviesController> {
+    
+    const url = `${originUrl}${id}/credits?language=${language}&region=${region}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: process.env.TMDB_Token,
+      },
+    };
+
+    const res = await fetch(url, options)
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return res;
+  };
+
+  //Get Movie Social ID by deifned id
+  async getMoviesSocialIDByID(
+    id: number,
+  ): Promise<MoviesController> {
+    
+    const url = `${originUrl}${id}/external_ids?language=${language}&region=${region}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: process.env.TMDB_Token,
+      },
+    };
+
+    const res = await fetch(url, options)
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return res;
+  };
+
+    //Get Movie Images by deifned id
+    async getMoviesImagesByID(
+      id: number,
+    ): Promise<MoviesController> {
+      
+      const url = `${originUrl}${id}/images`;
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: process.env.TMDB_Token,
+        },
+      };
+  
+      const res = await fetch(url, options)
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+  
+      return res;
+    };
+
+    //Get Movie Keywords by deifned id
+    async getMoviesKeywordsByID(
+      id: number,
+    ): Promise<MoviesController> {
+      
+      const url = `${originUrl}${id}/keywords`;
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: process.env.TMDB_Token,
+        },
+      };
+  
+      const res = await fetch(url, options)
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+  
+      return res;
+    };
+
+    //Get Movie Inclueded List by deifned id
+    async getMoviesIncludedListsByID(
+      id: number,
+    ): Promise<MoviesController> {
+      
+      const url = `${originUrl}${id}/lists`;
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: process.env.TMDB_Token,
+        },
+      };
+  
+      const res = await fetch(url, options)
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+  
+      return res;
+    };
+
+  //Get Movie Recommendations by deifned id
+  async getMoviesRecommendationsByID(
+    id: number,
+  ): Promise<MoviesController> {
+    
+    const url = `${originUrl}${id}/recommendations?language=${language}&region=${region}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: process.env.TMDB_Token,
+      },
+    };
+
+    const res = await fetch(url, options)
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return res;
+  };
+
+  //Get Movie Release Dates by deifned id
+  async getMoviesReleaseDatesByID(
+    id: number,
+  ): Promise<MoviesController> {
+    
+    const url = `${originUrl}${id}/release_dates?language=${language}&region=${region}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: process.env.TMDB_Token,
+      },
+    };
+
+    const res = await fetch(url, options)
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return res;
+  };
+
+  //Get Movie Reviews by deifned id
+  async getMoviesReviewsByID(
+    id: number,
+  ): Promise<MoviesController> {
+    
+    const url = `${originUrl}${id}/reviews?language=${language}&region=${region}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: process.env.TMDB_Token,
+      },
+    };
+
+    const res = await fetch(url, options)
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return res;
+  };
+
+  //Get Similar Movie by deifned id
+  async getMoviesSimilarByID(
+    id: number,
+  ): Promise<MoviesController> {
+    
+    const url = `${originUrl}${id}/similar?language=${language}&region=${region}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: process.env.TMDB_Token,
+      },
+    };
+
+    const res = await fetch(url, options)
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return res;
+  };
+
+  //Get Movie Translation Description by deifned id
+  async getMoviesTranslationByID(
+    id: number,
+  ): Promise<MoviesController> {
+    
+    const url = `${originUrl}${id}/translations?language=${language}&region=${region}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: process.env.TMDB_Token,
+      },
+    };
+
+    const res = await fetch(url, options)
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return res;
+  };
+
+  //Get Movie Videos by deifned id
+  async getMovieVideosByID(
+    id: number,
+  ): Promise<MoviesController> {
+    
+    const url = `${originUrl}${id}/videos?language=${language}&region=${region}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: process.env.TMDB_Token,
+      },
+    };
+
+    const res = await fetch(url, options)
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return res;
+  };
+
+  //Get Movie Watch Providers by deifned id
+  async getMovieWatchProviderByID(
+    id: number,
+  ): Promise<MoviesController> {
+    
+    const url = `${originUrl}${id}/providers?language=${language}&region=${region}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: process.env.TMDB_Token,
+      },
+    };
+
+    const res = await fetch(url, options)
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return res;
+  };
+
+  //Get Movie Lastest by deifned id
+  async getMoviesLastestByID(
+    id: number,
+  ): Promise<MoviesController> {
+    
+    const url = `${originUrl}${id}/latest?language=${language}&region=${region}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: process.env.TMDB_Token,
+      },
+    };
+
+    const res = await fetch(url, options)
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return res;
+  };
 }

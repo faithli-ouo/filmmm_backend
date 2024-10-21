1c604aa4c47df801aa6cb626b20acbe57fe29b78
root <128820090+faithli0929@users.noreply.github.com>
2024-10-21 15:16:42 +0800

    Relocate Movie dto into Movies Folder

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get,  HttpException,  HttpStatus,  Query, ValidationPipe } from '@nestjs/common';
+import { Controller, Get,  HttpException,  HttpStatus,  Query, } from '@nestjs/common';
 import { MoviesService } from './movies.service';
-import { MovieTypesDTO } from 'src/dto/movies_types_dto';
+import { MovieTypesDTO } from 'src/movies/dto/movies_types_dto';
 
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
   @Get()
-  async findAll(@Query(new ValidationPipe()) movieTypesDTO: MovieTypesDTO ): Promise<unknown> {
+  async findAll(@Query() movieTypesDTO: MovieTypesDTO ): Promise<unknown> {
     const type = movieTypesDTO.type
     try {
       const res = await this.moviesService.getMovies(type)
